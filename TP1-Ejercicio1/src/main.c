28b1220eadfc5a1cdf4d77c3adc30de1b0cb42b9
Fernanda Avila <eygleen.avila@alu.ing.unlp.edu.ar>
2026-09-07 23:27:11 -0300

    ejer1

--- TP1-Ejercicio1/src/main.c
+++ TP1-Ejercicio1/src/main.c
@@ -1,11 +1,30 @@
 //#define F_CPU 16000000UL
 #include <avr/io.h> // Definicion de Registros del microcontrolador
-#include <avr/interrupt.h>
 #include <util/delay.h> 
 
+
+void GPIO_Init(void){
+	DDRD &= ~((1 << PORTD2) & (1 << PORTD3)); // pulsadores (entrada)
+	PORTD |= (1 << PORTD2) & (1 << PORTD3); // Resistencias Pull-UP
+
+	DDRB |= (1 << PORTB0) & (1 << PORTB1); // Leds (salidas)
+	PORTB |= (1 << PORTB0) & (1 << PORTB1); // Estado inicial encendido
+}
 int main(void){
 
+	GPIO_Init();
+	uint8_t parpadeo = 0;
 	while (1){
+		if (!parpadeo){
+			if (!(PIND & (1 << PORTD2)) | !(PIND & (1 << PORTD3))){
+				parpadeo = 1;
+			}
+		}
+		else{
+			PORTB ^= (1 << PORTB1);
+			_delay_ms(250);
+			PORTB ^= (1 << PORTB0);
+		}
 
 	}
 }
--- TP1-Ejercicio1/src/vcd.h
+++
@@ -1,3 +0,0 @@
-#include <avr/io.h>
-#include "avr_mcu_section.h"
-const struct avr_mmcu_vcd_trace_t _mytrace[];
