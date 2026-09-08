15abeee28b9047cb8f03f2313683a80f51e0e6d6
Fernanda Avila <eygleen.avila@alu.ing.unlp.edu.ar>
2026-09-07 23:54:07 -0300

    o

--- TP1-Ejercicio1/.pio/build/ATmega328P/src/main.c
+++ TP1-Ejercicio1/.pio/build/ATmega328P/src/main.c
@@ -1,30 +1,43 @@
-//#define F_CPU 16000000UL
-#include <avr/io.h> // Definicion de Registros del microcontrolador
-#include <util/delay.h> 
-
+#define F_CPU 16000000UL // Necesario definirlo antes de <util/delay.h> para temporización exacta
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
 
 void GPIO_Init(void){
-	DDRD &= ~((1 << PORTD2) & (1 << PORTD3)); // pulsadores (entrada)
-	PORTD |= (1 << PORTD2) & (1 << PORTD3); // Resistencias Pull-UP
+	// Entradas: PD2 y PD3 (poner en '0' en DDRD)
+	DDRD &= ~((1 << DDD2) | (1 << DDD3));
 
-	DDRB |= (1 << PORTB0) & (1 << PORTB1); // Leds (salidas)
-	PORTB |= (1 << PORTB0) & (1 << PORTB1); // Estado inicial encendido
+	// Activar resistencias Pull-Up (poner en '1' en PORTD)
+	PORTD |= (1 << PORTD2) | (1 << PORTD3);
+
+	// Salidas: PB0 y PB1 (poner en '1' en DDRB)
+	DDRB |= (1 << DDB0) | (1 << DDB1);
+
+	// Estado inicial: ambos LEDs encendidos
+	PORTB |= (1 << PORTB0) | (1 << PORTB1);
 }
-int main(void){
 
+int main(void){
 	GPIO_Init();
-	uint8_t parpadeo = 0;
+	bool parpadeo = false;
+
 	while (1){
 		if (!parpadeo){
-			if (!(PIND & (1 << PORTD2)) | !(PIND & (1 << PORTD3))){
-				parpadeo = 1;
+			// Con Pull-up activo, presionar el pulsador envía un 0 lógico al pin
+			if (!(PIND & (1 << PIND2)) || !(PIND & (1 << PIND3))){
+				parpadeo = true;
 			}
 		}
 		else{
-			PORTB ^= (1 << PORTB1);
+			// Estado 1: PB0 encendido, PB1 apagado (250 ms)
+			PORTB = (PORTB & ~(1 << PORTB1)) | (1 << PORTB0);
 			_delay_ms(250);
-			PORTB ^= (1 << PORTB0);
-		}
 
+			// Estado 2: PB0 apagado, PB1 encendido (250 ms)
+			PORTB = (PORTB & ~(1 << PORTB0)) | (1 << PORTB1);
+			_delay_ms(250);
+		}
 	}
+
+	return 0;
 }
